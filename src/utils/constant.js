bf42b3168937cd831e95230920e14a2de3796419
Sai kumar <kamisettysaikumar57@gmail.com>
2024-02-23 09:39:04 +0530

    modified

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,4 +5,3 @@ export const MENU_URL=
 export const IMG_LINK="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
 export const CART_IMG="https://cdn-icons-png.flaticon.com/128/3144/3144456.png"
 export const SWIGGY_API="https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      
