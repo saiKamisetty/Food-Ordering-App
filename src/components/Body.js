77ec0f0bb7557da59643089d482082309f43262f
Sai kumar <kamisettysaikumar57@gmail.com>
2024-03-04 15:07:14 +0530

    M

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,20 +23,14 @@ const Body=()=>{
  
 
     const fetchData = async () => {
-        try {
             const Data = await fetch(SWIGGY_API);
-            if (!Data.ok) {
-                throw new Error('Failed to fetch data');
-            }
             const Raw_Data = await Data.json();
             console.log(Raw_Data);
     
             // Setting the list of restaurants and filtered list based on specific properties of the retrieved data
-            setListOfRestaurants(Raw_Data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants||Raw_Data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-            setFilteredList(Raw_Data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||Raw_Data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+            setListOfRestaurants(Raw_Data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || Raw_Data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants );
+            setFilteredList(Raw_Data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||Raw_Data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants );
+         
     }
     
     const onlineStatus = useOnlineStatus();
