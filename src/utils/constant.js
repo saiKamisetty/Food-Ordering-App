54dbe8b26a71675e83f9612fed58b7b09f5b7fd4
Sai kumar <kamisettysaikumar57@gmail.com>
2024-03-15 15:07:57 +0530

    modified

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,7 @@
 export const LOGO_LINK ="https://png.pngtree.com/png-clipart/20221110/original/pngtree-health-restaurant-logo-design-template-vector-picture-image_3607709.png"
-export const MENU_URL='https://corsproxy.org/?' + encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId="
-);
+export const MENU_URL='https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId='
 export const IMG_LINK="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
 export const CART_IMG="https://cdn-icons-png.flaticon.com/128/3144/3144456.png"
-export const SWIGGY_API='https://corsproxy.org/?' + encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-);
+export const SWIGGY_API="https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+ 
  
