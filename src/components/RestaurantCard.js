9ba80211f48c343e8d47e3cb3b395a71ea6cb6c6
Sai kumar <kamisettysaikumar57@gmail.com>
2024-04-10 12:29:52 +0530

    modified

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,10 +1,10 @@
 const RestaurantCard = ({restaurant}) => {
     const { name, cuisines, costForTwo, areaName } = restaurant.info;
-  
+   console.log(restaurant.info.cloudinaryImageId)
     return (
       <div className="w-[270px] h-[480px] m-3 bg-black-400 shadow-lg rounded-lg hover:bg-slate-300">
         <div className=" rounded-t-lg">
-          <img className="w-full p-3 h-56 " src={`https://res.cloudinary.com/swiggy/image/upload/${restaurant.info.cloudinaryImageId}`} alt={name} />
+          <img className="w-full p-3 h-56 " src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/${restaurant.info.cloudinaryImageId}`} alt={name} />
         </div>
         <div className="m-3 p-1">
           <h3 className=" font-bold text-xl mb-2">{restaurant.info.name}</h3>
